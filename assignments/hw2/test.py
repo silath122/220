796e1fe65ab8e2e81f2946128d0e1e15f27ccf79
silath122 <thomassl1@g.cofc.edu>
2022-01-28 22:34:55 -0500

    Completed hw2!

--- assignments/hw2/test.py
+++ assignments/hw2/test.py
@@ -1,3 +1,26 @@
 from tests.hw2 import test
 
-test.main()
+import math
+
+# Input: lower and upper ranges Output: sum of the squares
+# list of the rang
+# 4 * 4 * 4 specify how many times you multiply by the base
+# multiply it by the specific number of times
+
+def power():
+    print("Calculate the power of numbers.")
+    print()
+    base = eval(input("Enter the base: "))
+    exponent = eval(input("Enter the exponent: "))
+    j = 1
+    for i in range(1, exponent + 1):
+        j = j * base
+    print(base, "^", exponent, "=", j)
+power()
+
+
+
+
+
+
+
