982839df66b92f4a9db35be9e8bb1e64c56fc15d
silath122 <97462959+silath122@users.noreply.github.com>
2022-01-28 22:48:23 -0500

    Update hw2.py

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,64 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Siah Thomas>
+<hw2>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <This calculates the sum of all multiples of three, a multiplication table, area of a triangle, sum of squares,
+and the power function.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify that this assignment is entirely my own work.>
+
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    print("Calculate the sum of all multiples of three with a given number.")
+    print()
+    upper = eval(input("What is the upper bound? "))
+    my_range = list(range(upper, 0, -3))
+    three = sum(my_range)
+    print("the sum of threes is ", three)
 
 
 def multiplication_table():
-    pass
-
+    print(" A multiplication table for numbers 1 to 10.")
+    print()
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end="\t")
+        print("\n")
 
 def triangle_area():
-    pass
-
+    print("Calculate the area of a triangle.")
+    print()
+    a = eval(input("Enter the length of side a: "))
+    b = eval(input("Enter the length of side b: "))
+    c = eval(input("Enter the length of side c: "))
+    s = (a + b + c) / 2
+    air = (s - a) * (s - b) * (s - c)
+    airs = math.sqrt(s * air)
+    print("The area is ", airs)
 
 def sum_squares():
-    pass
-
+    print("Calculate the sum of the squares of the given lower and upper range.")
+    print()
+    lower = eval(input("Enter the lower range: "))
+    upper = eval(input("Enter the upper range: "))
+    j = 0
+    for i in list(range(lower, upper + 1)):
+        j = j + (i * i)
+    print(j)
 
 def power():
-    pass
+    print("Calculate the power of numbers.")
+    print()
+    base = eval(input("Enter the base: "))
+    exponent = eval(input("Enter the exponent: "))
+    j = 1
+    for i in range(1, exponent + 1):
+        j = j * base
+    print(base, "^", exponent, "=", j)
 
 
 if __name__ == '__main__':
