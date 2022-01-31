55c9722a5218f11ba77ca21be0361a42ee8ae79f
silath122 <97462959+silath122@users.noreply.github.com>
2022-01-31 15:11:22 -0500

    Update lab2.py

---
+++ labs/lab2/lab2.py
@@ -0,0 +1,20 @@
+import math
+
+def types_of_me():
+    n = eval(input("Enter the values to be entered: "))
+    rm = 0
+    har = 0
+    ge = 1
+    for i in range(n):
+        temp = eval(input("Enter a value: "))
+        rm = rm + (temp * temp)
+        rms = math.sqrt(rm / n)
+        har = har + (1 / temp)
+        harm = n / har
+        ge = ge * temp
+        geo = ge ** (1 / n)
+    print(round(rms, 3))
+    print(round(harm, 3))
+    print(round(geo, 3))
+
+types_of_me
