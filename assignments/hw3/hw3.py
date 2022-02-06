51c8ae448ab46cf704b78174c900a0cb61585648
silath122 <97462959+silath122@users.noreply.github.com>
2022-02-05 19:41:48 -0500

    Update hw3.py

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,35 +1,45 @@
-"""
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
-"""
-
-
-def average():
-    pass
-
-
-def tip_jar():
-    pass
-
-
-def newton():
-    pass
-
-
-def sequence():
-    pass
-
-
-def pi():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+"""                                                                                                                     
+Name: <Siah Thomas>                                                                                                     
+<hw3>.py                                                                                                                
+                                                                                                                        
+Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>           
+                                                                                                                        
+Certification of Authenticity:                                                                                          
+<I certify that this assignment is entirely my own work.>                                                               
+                                                                                                                        
+"""                                                                                                                     
+import math                                                                                                             
+                                                                                                                        
+def average():                                                                                                          
+    avg = eval(input("How many grades will you enter? "))                                                               
+    j = 0                                                                                                               
+    for i in range(avg):                                                                                                
+        temp = eval(input("Enter grade: "))                                                                             
+        j = j + temp                                                                                                    
+        ms = j / avg                                                                                                    
+    print("The average is ", round(ms, 1))                                                                              
+                                                                                                                        
+def tip_jar():                                                                                                          
+    total = 0                                                                                                           
+    for i in range(5):                                                                                                  
+        ask = eval(input("How much would you like to donate? "))                                                        
+        total = total + ask                                                                                             
+    print("total tips: ", round(total, 2))                                                                              
+                                                                                                                        
+def newton():                                                                                                           
+    t = eval(input("What number do you want to square root? "))                                                         
+    ap = eval(input("How many times should we improve the approximation? "))                                            
+    j = 0                                                                                                               
+    for i in range(ap):                                                                                                 
+        j = (j + (t / ap)) / 2                                                                                          
+    print("The square root is approximately", j)                                                                        
+                                                                                                                        
+def sequence():                                                                                                         
+     pass                                                                                                               
+                                                                                                                        
+def pi():                                                                                                               
+    pass                                                                                                                
+                                                                                                                        
+                                                                                                                        
+if __name__ == '__main__':                                                                                              
+    pass                                                                                                                
