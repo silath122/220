dedc434b8212b1b5fcb89df34838d03bf3c280ea
silath122 <thomassl1@g.cofc.edu>
2022-02-25 23:45:58 -0500

    Completed hw6!

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,45 +1,90 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Siah Thomas>
+<hw6>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify that this assignment is entirely my own work.>
+
 """
 
 
 def cash_converter():
-    pass
-
+    integer = eval(input("Enter an integer:"))
+    print("That is ${}.00".format(round(integer, 2)))
 
 def encode():
-    pass
+    message = input("Enter a message:")
+    key = eval(input("Enter a key: "))
+    code = ""
+    for ch in message:
+        unicode = ord(ch) + key
+        new_message = chr(unicode)
+        code = code+new_message
+    print(code)
 
 
 def sphere_area(radius):
-    pass
-
+    new_rad = float(radius)
+    area = 4 * 3.14 * (new_rad * new_rad)
+    return area
+rad_1 = eval(input("Enter the radius of the sphere: "))
+p = sphere_area(rad_1)
+print("The surface area of the sphere is", float(p))
 
 def sphere_volume(radius):
-    pass
-
+    new_rad = float(radius)
+    volume = (4 / 3) * 3.14 * (new_rad * new_rad * new_rad)
+    return volume
+rad_2 = eval(input("Enter the radius of the sphere: "))
+q = float(sphere_volume(rad_2))
+print("The volume of the sphere is", q)
 
 def sum_n(number):
-    pass
-
+    new_num = int(number)
+    add = 0
+    for each_number in range(1, new_num + 1):
+        add = add + each_number
+    return add
+new_num_2 = eval(input("Enter a natural number: "))
+r = int(sum_n(new_num_2))
+print("The sum of the first natural numbers are", r)
 
 def sum_n_cubes(number):
-    pass
-
+    new_cube_number = int(number)
+    add_2 = 0
+    for each_number in range(1, new_cube_number + 1):
+        add_2 = add_2 + (each_number * each_number)
+    return add_2
+new_cube_number_2 = eval(input("Enter a natural number: "))
+s = int(sum_n_cubes(new_cube_number_2))
+print("The sum of the cubes of the first natural number are", s)
 
 def encode_better():
-    pass
+    # Get message and key from user
+    message = input("Enter a message to be encoded: ")
+    key = input("Enter a key: ")
 
+    # Convert message and key to uppercase and remove spaces
+    message = message.upper().replace("", "")
+    key = key.upper().replace("", "")
 
-if __name__ == '__main__':
+    # Encoded message
+    encoded_message = ""
+
+    # Perform character shift
+    for i in range(len(message)):
+        letter_value = ord(message[i]) - 65
+        shift_amt = ord(key[i % len(key)]) - 65
+        new_letter = letter_value + shift_amt
+        new_letter = new_letter % 26
+        new_letter = new_letter + 65
+        new_letter = chr(new_letter)
+        encoded_message = encoded_message + new_letter
+
+    print(encoded_message)
+if __name__ =='__main__':
     # cash_converter()
     # encode()
     # res = sphere_area(13)
