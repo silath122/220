efe2e2acbaa22c669054c1b93be9a46e3a0e9f27
silath122 <thomassl1@g.cofc.edu>
2022-04-08 23:31:28 -0400

    Completed hw10!

---
+++ assignments/hw10/sphere.py
@@ -0,0 +1,20 @@
+
+import graphics
+import math
+
+class Sphere:
+    def __init__(self, radius):
+        self.radius = float(radius)
+
+    def get_radius(self):
+        return self.radius
+
+    def surface_area(self):
+        return 4 * math.pi * (self.radius ** 2)
+
+    def volume(self):
+        return (4/3) * math.pi * (self.radius ** 3)
+
+
+
+
