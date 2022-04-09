478c30f2b311f0376d711215e63830627c03cdc0
silath122 <97462959+silath122@users.noreply.github.com>
2022-04-08 23:37:23 -0400

    Create face.py

---
+++ assignments/hw10/face.py
@@ -0,0 +1,69 @@
+from graphics import Circle, Line, Polygon
+
+
+class Face:
+    def __init__(self, window, center, size):
+        self.shock_mouth = None
+        self.happy_mouth = None
+        eye_size = 0.15 * size
+        eye_off = size / 3.0
+        mouth_size = 0.8 * size
+        mouth_off = size / 2.0
+        self.window = window
+        self.head = Circle(center, size)
+        self.head.draw(window)
+        self.left_eye = Circle(center, eye_size)
+        self.left_eye.move(-eye_off, -eye_off)
+        self.right_eye = Circle(center, eye_size)
+        self.right_eye.move(eye_off, -eye_off)
+        self.left_eye.draw(window)
+        self.right_eye.draw(window)
+        point_1 = center.clone()
+        point_1.move(-mouth_size / 2, mouth_off)
+        point_2 = center.clone()
+        point_2.move(mouth_size / 2, mouth_off)
+        self.mouth = Line(point_1, point_2)
+        self.mouth.draw(window)
+
+
+    def smile(self, window, center, size):
+        eye_size = 0.15 * size
+        eye_off = size / 3.0
+        mouth_size = 0.8 * size
+        mouth_off = size / 2.0
+        self.window = window
+        self.head = Circle(center, size)
+        self.head.draw(window)
+        self.left_eye = Circle(center, eye_size)
+        self.left_eye.move(-eye_off, -eye_off)
+        self.right_eye = Circle(center, eye_size)
+        self.right_eye.move(eye_off, -eye_off)
+        self.left_eye.draw(window)
+        self.right_eye.draw(window)
+        point_1 = center.clone()
+        point_1.move(-mouth_size / 2, mouth_off)
+        point_2 = center.clone()
+        point_2.move(mouth_size / 2, mouth_off)
+        point_3 = center.clone()
+        point_3.move(0, mouth_off - 1)
+        self.happy_mouth = Polygon(point_1, point_2, point_3)
+        self.happy_mouth.draw(window)
+
+    def shock(self, window, center, size):
+        eye_size = 0.15 * size
+        eye_off = size / 3.0
+        self.window = window
+        self.head = Circle(center, size)
+        self.head.draw(window)
+        self.left_eye = Circle(center, eye_size)
+        self.left_eye.move(-eye_off, -eye_off)
+        self.right_eye = Circle(center, eye_size)
+        self.right_eye.move(eye_off, -eye_off)
+        self.left_eye.draw(window)
+        self.right_eye.draw(window)
+        self.shock_mouth = Circle(center, eye_size)
+        self.shock_mouth.move(0, eye_size / 3)
+
+
+    def wink(self):
+        pass
