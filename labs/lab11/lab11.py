4d166c382eaf882ffcf9bb44e98328cf3d34caec
silath122 <thomassl1@g.cofc.edu>
2022-04-11 19:43:50 -0400

    Completed lab 11!

--- labs/lab11/lab11.py
+++ labs/lab11/lab11.py
@@ -7,18 +7,19 @@ def main():
 # main window
     win= GraphWin('Three Door Game II', 1000, 750)
     win.setCoords(0.0, 0.0, 10.0, 10.0)
+    win.setBackground('Lightblue1')
 
     # Door 1 - draw
     door_1 = Door(Rectangle(Point(1.0, 0.0), Point(3.0, 6.0)), 'Door 1')
-    door_1.color_door('Brown')
+    door_1.color_door('Sienna')
     door_1.draw(win)
     # Door 2 - draw
     door_2 = Door(Rectangle(Point(4.0, 0.0), Point(6.0, 6.0)), 'Door 2')
-    door_2.color_door('Brown')
+    door_2.color_door('Sienna')
     door_2.draw(win)
     # Door 3 - draw
     door_3 = Door(Rectangle(Point(7.0, 0.0), Point(9.0, 6.0)), 'Door 3')
-    door_3.color_door('Brown')
+    door_3.color_door('Sienna')
     door_3.draw(win)
 
     # draw quit button
@@ -36,9 +37,19 @@ def main():
     loses_box = Rectangle(Point(1.5, 8.5), Point(2.5, 9.0))
     loses_box.draw(win)
 
+    # play again message and changing message
+    play_again_text = Text(Point(5.0, 7.00), 'Click to to guess which is the secret door!')
+    play_again_text.draw(win)
+    diff_message = Text(Point(5.0, 7.50), 'I have a secret door.')
+    diff_message.draw(win)
+
     # accum for loops wins and loses
     wins = 0
     loses = 0
+    wins_text = Text(Point(1.0, 8.75), wins)
+    wins_text.draw(win)
+    loses_text = Text(Point(2.0, 8.75), loses)
+    loses_text.draw(win)
 
     pt = win.getMouse()
     while not quit_button.is_clicked(pt):
@@ -46,21 +57,29 @@ def main():
         random_door = randint(0, len(doors) - 1)
         pt = win.getMouse()
         if doors[random_door].is_clicked(pt):
+            doors[random_door].color_door("Green")
+            diff_message.setText('you win!')
+            play_again_text.setText('Click to play again!')
+            wins += 1
+            wins_text.setText(wins)
+            win.getMouse()
+            for each_door in doors:
+                each_door.color_door('Sienna')
+            pt = win.getMouse()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    win.getMouse()
-    win.close()
-
-main()
+        else:
+            if doors[random_door].is_clicked(pt) == False:
+                for each_door in doors:
+                    each_door.color_door('Red')
+                doors[random_door].color_door("Green")
+                diff_message.setText('incorrect, try again!')
+                play_again_text.setText('Click to play again!')
+                loses += 1
+                loses_text.setText(loses)
+                win.getMouse()
+                for each_door in doors:
+                    each_door.color_door('Sienna')
+                pt = win.getMouse()
+    pt = win.getMouse()
+    if quit_button.is_clicked(pt):
+        win.close()
